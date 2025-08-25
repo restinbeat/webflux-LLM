f609ab7b1e8d94fc7b44b41b485354a2e2fd5283
restinbeat <thek226@gmail.com>
2025-08-25 09:34:51 +0900

    initial commit

---
+++ src/main/java/com/example/webflux_LLM/WebfluxLlmApplication.java
@@ -0,0 +1,13 @@
+package com.example.webflux_LLM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxLlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxLlmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webflux_LLM/WebfluxLlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webflux_LLM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxLlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
