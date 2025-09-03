1313a987ed1e53c2d27a13dd5396713edc6c1c6e
restinbeat <thek226@gmail.com>
2025-09-03 14:13:52 +0900

    test: mono test example

--- src/test/java/com/example/webflux_LLM/chapter2/BasicFluxMonoTest.java
+++ src/test/java/com/example/webflux_LLM/chapter2/BasicFluxMonoTest.java
@@ -2,12 +2,13 @@ package com.example.webflux_LLM.chapter2;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public class BasicFluxMonoTest {
 
     @Test
     public void testBasicFluxMono() {
-        Flux.<Integer>just(1, 2, 3, 4, 5)
+        Flux.<Integer>just(1,2,3,4,5)
             .map(data -> data * 2)
             .filter(data -> data % 4 == 0)
             .subscribe(data -> System.out.println("Flux가 구독한 data! = " + data));
@@ -18,5 +19,10 @@ public class BasicFluxMonoTest {
 
         // Mono 0개 1부터 1개의 데이터만 방출할 수 있는 객체 -> optional 정도
         // Flux 0개 이상의 데이터를 방출할 수 있는 객체 -> List, Stream 0개 이상의 데이터 방출
+
+        Mono.<Integer>just(2)
+            .map(data -> data * 2)
+            .filter(data -> data % 4 == 0)
+            .subscribe(data -> System.out.println("Mono 구독한 data! = " + data));
     }
 }
